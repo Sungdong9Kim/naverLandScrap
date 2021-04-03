42c1834f96869a167723ae4b3f3fd63618119e68
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-04-03 23:40:34 +0900

    feat : add code for inputing keyword and clicking for searching

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(3)
+#
+# driver.get('https://land.naver.com/')
+#
+# driver.find_element_by_css_selector('.search_input sta_search_text sta_text_ovr').send_keys('압구정 현대')
 
 driver.get('https://land.naver.com/')
+#
+driver.find_element_by_css_selector('input#queryInputHeader').send_keys('압구정 현대')
+driver.find_element_by_css_selector('a.search_button').click()
 
-driver.find_element_by_css_selector('queryInputHeader').send_keys('압구정 현대')
+driver.find_element_by_css_selector('div.title').click()
+
+driver.find_element_by_css_selector('button.list_filter_btn').click()
