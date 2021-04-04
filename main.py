c15430ee87f5a9127ec63ccb520608c4482b374b
Sungdong9Kim <sungdong9.kim@gmail.com>
2021-04-04 23:28:56 +0900

    fix : modify some code

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ driver.find_element_by_css_selector('a.search_button').click()
 driver.find_element_by_css_selector('div.title').click()
 
 driver.find_element_by_css_selector('button.list_filter_btn').click()
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    time.sleep(0.5)
+
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
